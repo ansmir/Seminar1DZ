6b37657653a5192cdb3c5a2bb2918a27b9e1f139
AnSmir <nightelf35@yandex.ru>
2022-12-06 19:29:25 +0300

    even or uneven

---
+++ task006_even_uneven/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+Console.Write("Enter your number: ");
+int numbera = int.Parse(Console.ReadLine());
+
+if (numbera % 2 == 0)
+{
+    Console.WriteLine("true");
+}
+else 
+{
+    Console.WriteLine("false");
+}
