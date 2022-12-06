738c2259a5398041298ba03f2266ab947dafe026
AnSmir <nightelf35@yandex.ru>
2022-12-06 18:25:31 +0300

    max between 2 and 10

--- task002_MAX/Program.cs
+++ task002_MAX/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
+// a = 5; b = 7 -> max = 7git remote add origin https://github.com/ansmir/Seminar1DZ.git
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-int a = 5;
-int b = 7;
+int a = 2;
+int b = 10;
 
 int max = a;
 
